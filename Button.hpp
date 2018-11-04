6b0a0fa27e8751a41b8b8cdeb7453ac968d86af1
ThePatman3 <patrick.vanderbend@student.hu.nl>
2018-11-04 09:14:03 +0100

    Better description of N

--- Button.hpp
+++ Button.hpp
@@ -3,7 +3,7 @@
 
 #include "IButtonListener.hpp"
 
-template<int N> // N is the amount of listeners
+template<int N> // N is the max amount of listeners
 class Button{
 public:
 	void SetListener(IButtonListener* Listener);
