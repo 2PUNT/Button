60194549c7eb1a1e0dd3232f2563c9cde1b6a383
MartyVos <marty.vos@student.hu.nl>
2018-11-22 14:05:27 +0100

    Added Doxygen Documentation

--- lib/Button.hpp
+++ lib/Button.hpp
@@ -13,18 +13,30 @@ class Button: public rtos::task<>{
 	IButtonListener* Listeners[N];
 	int listenersSize;
 	
-	
 public:
+	///@fn Button::Button(hwlib::target::pin_in ButtonPin, int ButtonID, const unsigned int priority, const char* name)
+    ///@brief The constructor for the Button class.
+    ///@details This constructor creates a Button object. 
+    ///@param ButtonPin The input pin to the button.
+    ///@param ButtonID The ID of the button.
+	///@param priority The priority of the listener.
+	///@param name The name of the listener.
 	Button(hwlib::target::pin_in ButtonPin, int ButtonID, const unsigned int priority, const char* name):
 	task(priority, name), ButtonID(ButtonID), ButtonClock(this, 50'000, "ButtonClock"),
 	ButtonPin(ButtonPin), listenersSize(0){};
 	
+	///@fn void Button::SetButtonListener(IButtonListener *Listener)
+    ///@brief Set ButtonListener.
+    ///@details This method sets the listeners.
+    ///@param Listener A listener.
 	void SetButtonListener(IButtonListener* Listener){
 		if(listenersSize == N){ hwlib::cout << "Maximum listeners reached!\n"; return;}
 		Listeners[listenersSize] = Listener;
 		listenersSize++;
 	}
 	
+	///@fn void Button::main()
+    ///@brief The main of the Button task.
 	void main(){
 		while(true){
 			wait(ButtonClock);
--- lib/IButtonListener.hpp
+++ lib/IButtonListener.hpp
@@ -3,7 +3,12 @@
 
 class IButtonListener{
 public:
+	///@fn virtual void IButtonListener::ButtonPressed(int ButtonID)=0
+    ///@param ButtonID A Button ID.
 	virtual void ButtonPressed(int ButtonID)=0;
+	
+	///@fn virtual void IButtonListener::ButtonReleased(int ButtonID)=0
+    ///@param ButtonID A Button ID.
 	virtual void ButtonReleased(int ButtonID)=0;
 };
 
